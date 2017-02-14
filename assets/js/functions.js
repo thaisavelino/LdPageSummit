64a45b6757291dc78c6d6d8a76b4d77b370dd488
thaisavelino <t.avelino@gmail.com>
2017-02-14 19:58:41 -0200

    iniciando o slider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,8 @@
-$( document ).ready(function() {
-
-  // começar!
-
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    slideWidth: 5000,
+    minSlides: 2,
+    maxSlides: 4,
+    slideMargin: 10
+  });
 });
